87f3f2735023c2c29aa44b0d9c04eb23ca01c1b4
Singh <nishant.singh@softwareone.com>
2025-05-15 11:52:15 +0800

    Resolved merge conflict in tf/manifest.json

--- tf/locals.tf
+++ tf/locals.tf
@@ -1,8 +1,8 @@
-locals {
-  landing_zone_manifest = templatefile("${path.module}/manifest.json", {
-    log_archive_account_id = aws_organizations_account.log_archive.id
-    audit_account_id       = aws_organizations_account.audit.id
-    kms_key_arn            = "arn:aws:kms:ap-southeast-1:021668988431:key/19436235-1eeb-4fa8-b84b-0d95dbe4d87e"
-    region                 = "ap-southeast-1a"
-  })
-}
+#locals {
+#  landing_zone_manifest = templatefile("${path.module}/manifest.json", {
+#    log_archive_account_id = aws_organizations_account.log_archive.id
+#    audit_account_id       = aws_organizations_account.audit.id
+#    kms_key_arn            = "arn:aws:kms:ap-southeast-1:021668988431:key/19436235-1eeb-4fa8-b84b-0d95dbe4d87e"
+#    region                 = "ap-southeast-1a"
+#  })
+#}
