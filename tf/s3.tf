eaf40d945e884fb448cb0d6e1bafb4bd6d7bcc69
Singh <nishant.singh@softwareone.com>
2025-07-08 14:44:16 +0800

    ct

--- tf/s3.tf
+++ tf/s3.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket         = "388762879711-organizationcloudtrail"
     key            = "new/tfstate"
-    region         = "ap-southeast-1"
+    region         = "us-east-1"
     #dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
