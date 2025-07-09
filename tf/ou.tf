555ad8a1285ad401fc7fc04e6d87fa9374de7fbf
Singh <nishant.singh@softwareone.com>
2025-07-09 18:47:36 +0800

    ct

--- tf/ou.tf
+++ tf/ou.tf
@@ -2,14 +2,14 @@ data "aws_organizations_organization" "main" {}
 
 resource "aws_organizations_account" "log_archive" {
   name      = "logging"
-  email     = "testusr538@gmail.com"
+  email     = "tuse82013@gmail.com"
   role_name = "OrganizationAccountAccessRole"
   parent_id = data.aws_organizations_organization.main.roots[0].id
 }
 
 resource "aws_organizations_account" "audit" {
   name      = "auditing"
-  email     = "tu603717@gmail.com"
+  email     = "utest1341@gmail.com"
   role_name = "OrganizationAccountAccessRole"
   parent_id = data.aws_organizations_organization.main.roots[0].id
 }
--- tf/s3.tf
+++ tf/s3.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "my-uniquea-tfapse"
-    key            = "newest/tfstate"
+    key            = "newests/tfstate"
     region         = "ap-southeast-1"
     #dynamodb_table = "terraform-state-lock"
     encrypt        = true
