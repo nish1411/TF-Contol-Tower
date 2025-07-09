1133ce76f0ff1e4e138c579e25f8700f849e8686
Singh <nishant.singh@softwareone.com>
2025-07-09 11:49:49 +0800

    ct

--- tf/ou.tf
+++ tf/ou.tf
@@ -1,26 +1,25 @@
 data "aws_organizations_organization" "main" {}
-#
-#resource "aws_organizations_account" "log_archive" {
-#  name      = "log"
-#  email     = "nishsingh548@gmail.com"
-#  role_name = "OrganizationAccountAccessRole"
-#  parent_id = data.aws_organizations_organization.main.roots[0].id
-#}
-#
-#resource "aws_organizations_account" "audit" {
-#  name      = "aud"
-#  email     = "dikshsingh332@gmail.com"
-#  role_name = "OrganizationAccountAccessRole"
-#  parent_id = data.aws_organizations_organization.main.roots[0].id
-#}
-#
-#
-#output "log_archive_account_id" {
-#  value = aws_organizations_account.log_archive.id
-#}
-#
-#output "audit_account_id" {
-#  value = aws_organizations_account.audit.id
-#}
-#
-#
+
+resource "aws_organizations_account" "log_archive" {
+  name      = "logging"
+  email     = "testusr538@gmail.com"
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = data.aws_organizations_organization.main.roots[0].id
+}
+
+resource "aws_organizations_account" "audit" {
+  name      = "auditing"
+  email     = "tu603717@gmail.com"
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = data.aws_organizations_organization.main.roots[0].id
+}
+
+
+output "log_archive_account_id" {
+  value = aws_organizations_account.log_archive.id
+}
+
+output "audit_account_id" {
+  value = aws_organizations_account.audit.id
+}
+
