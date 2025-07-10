42acaad07c5158960bce6569cbef56c6c00d0633
Singh <nishant.singh@softwareone.com>
2025-07-10 17:55:15 +0800

    ct

--- modules/ou/main.tf
+++ modules/ou/main.tf
@@ -17,6 +17,6 @@ resource "aws_organizations_organizational_unit" "organizational_unit" {
 resource "aws_organizations_organizational_unit" "organizational_unit2" {
   count     = var.parent_ou_name == "" ? 1 : 0
   name      = var.ou_name
-  parent_id = data.aws_organizations_organization.main.roots[0].id
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
