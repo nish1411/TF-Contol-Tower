eac425e58d460d6aacba6a4485c42018acfb6e6f
Singh <nishant.singh@softwareone.com>
2025-07-10 18:46:44 +0800

    ct

--- account/ou.tf
+++
@@ -1,7 +0,0 @@
-module "accounts" {
-  for_each          = { for acc in var.accounts : "${acc.ou}-${acc.account_name}" => acc }
-  source            = "../modules/account"
-  account_name      = each.value.account_name
-  account_email     = each.value.account_email
-  ou_name           = each.value.ou_name
-}
