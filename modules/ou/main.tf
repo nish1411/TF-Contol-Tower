aaed96a5c2b14082dee38f56c2eb1e8fa5eae86c
Singh <nishant.singh@softwareone.com>
2025-07-12 14:57:16 +0800

    ct

--- modules/ou/main.tf
+++ modules/ou/main.tf
@@ -1,15 +1,15 @@
-
+data "aws_organizations_organization" "main" {}
 
 data "aws_organizations_organizational_unit" "top_ou" {
   count     = var.parent_ou_name != null ? 1 : 0
   name      = var.parent_ou_name
-  parent_id = data.aws_organizations_organization.org.roots[0].id
+  parent_id = data.aws_organizations_organization.main.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "organizational_unit" {
   count     = var.parent_ou_name != "" ? 1 : 0
   name      = var.ou_name
-  parent_id = var.parent_ou_name != null ? data.aws_organizations_organizational_unit.top_ou[0].id : data.aws_organizations_organization.org.roots[0].id
+  parent_id = var.parent_ou_name != null ? data.aws_organizations_organizational_unit.top_ou[0].id : data.aws_organizations_organization.main.roots[0].id
 }
 
 
