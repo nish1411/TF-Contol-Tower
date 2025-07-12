fcda7b5d92cd67487979973e57a2b75cf0622e95
Singh <nishant.singh@softwareone.com>
2025-07-12 14:38:11 +0800

    ct

--- modules/account/variables.tf
+++ modules/account/variables.tf
@@ -1,15 +1,16 @@
+data "aws_organizations_organization" "org" {}
 
-variable "account_name" {
-    type =  string
-    default = ""
+data "aws_organizations_organizational_unit" "top_ou" {
+  count     = var.parent_ou_name != null ? 1 : 0
+  name      = var.parent_ou_name
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
-variable "account_email" {
-    type =  string
-    default = ""
+resource "aws_organizations_organizational_unit" "organizational_unit" {
+  count     = var.parent_ou_name != "" ? 1 : 0
+  name      = var.ou_name
+  parent_id = var.parent_ou_name != null ? data.aws_organizations_organizational_unit.top_ou[0].id : data.aws_organizations_organization.org.roots[0].id
 }
 
-variable "ou_name" {
-    type =  string
-    default = ""
-}
+
+
