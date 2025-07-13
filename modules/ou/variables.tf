56c6519a30992c085ca015a4283abb199dc8166d
Singh <nishant.singh@softwareone.com>
2025-07-13 12:54:29 +0800

    nwq

--- modules/ou/variables.tf
+++ modules/ou/variables.tf
@@ -1,6 +1,6 @@
 variable "organizational_units" {
   type = map(object({
-    child_ous = optional(list(string))
+    child_ous = optional(list(string), [])
   }))
   default     = {}
   description = "organizational_unit_mapping"
